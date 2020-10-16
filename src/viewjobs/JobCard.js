20277183f9ae69a4c00926a05e4b0f19274cc1ec
denvermullets <devmullets@gmail.com>
2020-10-16 15:36:25 -0400

    :sparkles: can save / ignore / mark as applied - also hides senior roles

--- src/viewjobs/JobCard.js
+++ src/viewjobs/JobCard.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 const JobCard = (props) => {
-  const { applied, company_name, company_page, connection, interested, job_description, job_source, job_title, job_url, location, remind_me, salary_info } = props.job
-
+  const { id, applied, company_name, company_page, connection, job_description, job_source, job_title, job_url, location, remind_me, salary_info } = props.job
+  const jobApplied = applied ? ' applied ' : " "
+  const jobSaved = remind_me ? 'saved' : ""
   return (
     <>
 
-        <ul className="job-list">
-          <li className="job-preview">
+        <ul className={"job-list"}>
+        {/* <ul className={"job-list" + jobApplied + jobSaved }> */}
+          <li className={"job-preview" + jobApplied + jobSaved }>
             <h2 className="job-title">
-             <a href={job_url} target="_blank" >{ job_title ? job_title : null }</a>
+             <a href={job_url} target="_blank" rel="noopener noreferrer" >{ job_title ? job_title : null }</a>
             </h2>
             <h3 className="job-title">
              { salary_info ? salary_info : null }
@@ -17,7 +19,7 @@ const JobCard = (props) => {
             <h3 className="company">
               {
                 company_page ? 
-                  <a href={ company_page ? `${company_page}people` : null } target="_blank" className="company" >
+                  <a href={ company_page ? `${company_page}people` : null } target="_blank" className="company" rel="noopener noreferrer" >
                     { location ? `${company_name} - ${location}` : company_name }</a>
                 : location ? `${company_name} - ${location}` : company_name 
               }
@@ -28,18 +30,18 @@ const JobCard = (props) => {
               { connection ? connection : null }
             </h5>
             <p>{ job_description ? job_description : null }</p>
-            <a href={job_url} target="_blank" >
+            <a href={job_url} target="_blank" rel="noopener noreferrer" >
               <button className="btn-apply">
                 View
               </button>
             </a>
-            <button className="btn-apply">
+            <button className="btn-apply" onClick={() => props.saveJob(id, props.index)}>
               Save
             </button>
-            <button className="btn-apply">
+            <button className="btn-apply" onClick={() => props.appliedForJob(id, props.index)}>
               Applied
             </button>
-            <button className="btn-apply">
+            <button className="btn-apply" onClick={() => props.removeJob(id)}>
               Ignore
             </button>
           </li>
--- src/viewjobs/ViewJobs.js
+++ src/viewjobs/ViewJobs.js
@@ -14,12 +14,63 @@ const ViewJobs = () => {
   const [ jobs, setJobs ] = useState([])
   const [ loading, setLoading ] = useState(false)
 
+  const ignoreJob = (id) => {
+    fetch(`http://localhost:3000/openings/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({"interested": "false"}),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+      .then(response => response.json())
+  }
+
+  const saveJob = (id, index) => {
+    setLoading(!loading)
+    fetch(`http://localhost:3000/openings/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({"remind_me": !jobs[index].remind_me}),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+      .then(response => response.json())
+    let savedJobs = [...jobs]
+    savedJobs[index] = {
+      ...savedJobs[index], remind_me: !savedJobs[index].remind_me
+    }
+
+    setJobs(savedJobs)
+    console.log('job marked as saved')
+
+  }
+
+  const appliedForJob = (id, index) => {
+    setLoading(!loading)
+    fetch(`http://localhost:3000/openings/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({"applied": !jobs[index].applied}),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+      .then(res => res.json())
+
+    let appliedJobs = [...jobs]
+    appliedJobs[index] = {
+      ...appliedJobs[index], applied: !appliedJobs[index].applied
+    }
+
+    setJobs(appliedJobs)
+    console.log('marked as applied')
+  }
+
+  const removeJob = (id) => {
+    setLoading(!loading)
+    ignoreJob(id)
+    let updatedJobs = jobs.filter((single_job) => single_job.id !== id )
+    setJobs(updatedJobs)
+  }
+
   const refreshLinked = () => {
     setLoading(!loading)
     fetch('http://localhost:3000/linkedin')
       .then(response => response.json())
       .then(joberinos => setJobs(joberinos))
-      // .then(setLoading(!loading))
   }
 
   const refreshIndeed = () => {
@@ -27,10 +78,12 @@ const ViewJobs = () => {
     fetch('http://localhost:3000/indeed')
       .then(response => response.json())
       .then(joberinos => {
-        // setLoading(!loading)
         setJobs(joberinos)
       })
-      .catch(err => alert("you'll need to refresh the page and try again, indeed has agressive anti scrape methods"))
+      .catch(err => {
+        setLoading(!loading)
+        alert("you'll need to refresh the page and try again, indeed has a lot of popups that mess with the scrape method sometimes.")})
+
   }
   
   useEffect(() => {
@@ -43,6 +96,12 @@ const ViewJobs = () => {
     setLoading(!loading)
   }, [jobs]);
 
+  // // TODO: we are automatically filtering out senior roles, eventually will make this an option
+  let filteredJobs = jobs.filter(single_job => 
+    !single_job.job_title.toLowerCase().includes('senior') && !single_job.job_title.toLowerCase().includes('sr') && !single_job.job_title.toLowerCase().includes('lead')
+    )
+
+
   return (
     <>
       <header className="masthead">
@@ -51,24 +110,29 @@ const ViewJobs = () => {
             Open Positions
           </h1>
           <span className="subheading">
-            Current listings for full stack developer jobs.
+            Current listings for full stack developer jobs. NO senior / sr / lead listings.
           </span> <br />
           <button className='btn-refresh' onClick={() => refreshLinked()} >Refresh Linkedin</button>
           <button className='btn-refresh' onClick={() => refreshIndeed()} >Refresh Indeed</button>
+          <br /> {filteredJobs.length} jobs being displayed.
         </div>
       </header>
       
         { loading ? <BarLoader
             css={override}
             size={50}
+            width={400}
             color={"#393a5f"}
         /> 
         : null }
       
-
-      { jobs ? jobs.map(job => 
+      { filteredJobs ? filteredJobs.map((job, index) => 
           <JobCard 
             job={job}
+            index={index}
+            saveJob={saveJob}
+            removeJob={removeJob}
+            appliedForJob={appliedForJob}
             key={job.id}
           /> )
           :
