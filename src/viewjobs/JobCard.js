0bc6aa3a169edc676827288f7790211b53e05bfe
denvermullets <devmullets@gmail.com>
2020-10-16 16:10:16 -0400

    :recycle: fix urls for job cards

--- src/viewjobs/JobCard.js
+++ src/viewjobs/JobCard.js
@@ -2,16 +2,15 @@ import React from 'react';
 
 const JobCard = (props) => {
   const { id, applied, company_name, company_page, connection, job_description, job_source, job_title, job_url, location, remind_me, salary_info } = props.job
-  const jobApplied = applied ? ' applied ' : " "
-  const jobSaved = remind_me ? 'saved' : ""
+  let jobApplied = applied ? ' applied ' : " "
+  let jobSaved = remind_me ? 'saved' : ""
   return (
     <>
 
         <ul className={"job-list"}>
-        {/* <ul className={"job-list" + jobApplied + jobSaved }> */}
           <li className={"job-preview" + jobApplied + jobSaved }>
             <h2 className="job-title">
-             <a href={job_url} target="_blank" rel="noopener noreferrer" >{ job_title ? job_title : null }</a>
+             <a href={job_source + job_url} target="_blank" rel="noopener noreferrer" >{ job_title ? job_title : null }</a>
             </h2>
             <h3 className="job-title">
              { salary_info ? salary_info : null }
@@ -19,7 +18,7 @@ const JobCard = (props) => {
             <h3 className="company">
               {
                 company_page ? 
-                  <a href={ company_page ? `${company_page}people` : null } target="_blank" className="company" rel="noopener noreferrer" >
+                  <a href={ company_page ? `${job_source + company_page}people` : null } target="_blank" className="company" rel="noopener noreferrer" >
                     { location ? `${company_name} - ${location}` : company_name }</a>
                 : location ? `${company_name} - ${location}` : company_name 
               }
@@ -30,7 +29,7 @@ const JobCard = (props) => {
               { connection ? connection : null }
             </h5>
             <p>{ job_description ? job_description : null }</p>
-            <a href={job_url} target="_blank" rel="noopener noreferrer" >
+            <a href={job_source + job_url} target="_blank" rel="noopener noreferrer" >
               <button className="btn-apply">
                 View
               </button>
